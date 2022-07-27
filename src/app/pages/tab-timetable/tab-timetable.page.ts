8220a1402dd5cec6ee3a010b4bff6b132b5816f9
baffioso <baffyx@gmail.com>
2022-07-27 22:14:25 +0200

    work on timetable

--- src/app/pages/tab-timetable/tab-timetable.page.ts
+++ src/app/pages/tab-timetable/tab-timetable.page.ts
@@ -3,8 +3,24 @@ import { DayWithRelations } from '@app/interfaces/entities-with-releation';
 import { StoreService } from '@app/store/store.service';
 import { SegmentCustomEvent } from '@ionic/angular';
 import { combineLatest, Observable } from 'rxjs';
-import { filter, map, pluck } from 'rxjs/operators';
+import { filter, map, pluck, tap } from 'rxjs/operators';
 import { TimetableStateService } from './state/timetable-state.service';
+
+interface StageTimetables {
+  stage: string;
+  timetables: {
+    name: string;
+    start_time: string;
+    end_time: string;
+  }[];
+}
+
+interface TimeSpan {
+  minStartTime: number;
+  maxEndTime: number;
+  intervalInHours: number;
+}
+
 
 @Component({
   selector: 'app-tab-timetable',
@@ -14,11 +30,33 @@ import { TimetableStateService } from './state/timetable-state.service';
 })
 export class TabTimetablePage implements OnInit {
 
+  // https://www.bennadel.com/blog/3961-having-fun-with-the-horizontal-usage-of-position-sticky-in-angular-11-0-5.htm
+
   days$: Observable<DayWithRelations[]>;
   events$: Observable<DayWithRelations['event']>;
   selectedDayId$: Observable<string>;
   selectedEventId$: Observable<string>;
 
+  timeLabels: { time: string, min: number }[] = [...Array(24).keys()].map(x => ({ time: `${x}:00`, min: x * 60 }));
+
+  stageTimetables: StageTimetables[] = [
+    {
+      stage: 'Main',
+      timetables: [
+        { name: 'DJ Aligator', start_time: '2023-06-02 21:00', end_time: '2023-06-02 23:00' },
+        { name: 'The Whitest Boy Alive', start_time: '2023-06-02 23:00', end_time: '2023-06-03 01:00' },
+        { name: 'Jeff Mills', start_time: '2023-06-03 01:00', end_time: '2023-06-03 04:00' },
+      ]
+    },
+    {
+      stage: 'Woods',
+      timetables: [
+        { name: 'Snoop Dog', start_time: '2023-06-02 20:30', end_time: '2023-06-02 21:30' },
+        { name: 'Cran Berries', start_time: '2023-06-02 22:00', end_time: '2023-06-02 23:30' },
+        { name: 'Kelly Family', start_time: '2023-06-03 0:00', end_time: '2023-06-03 05:35' },
+      ]
+    },
+  ]
 
   constructor(
     private store: StoreService,
@@ -26,8 +64,16 @@ export class TabTimetablePage implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.days$ = this.store.days$;
+
+    console.log(this.normalize(this.stageTimetables))
+
+
+    this.days$ = this.store.days$.pipe(
+      tap(console.log)
+    );
+
     this.selectedDayId$ = this.timetableStateService.selectedDay$
+
     this.events$ = combineLatest([
       this.days$,
       this.timetableStateService.selectedDay$
@@ -36,6 +82,27 @@ export class TabTimetablePage implements OnInit {
       map(([days, selectedDay]) => days.find(day => day.id === selectedDay)),
       pluck('event')
     );
+
+  }
+
+
+  normalize(stageTimetables: StageTimetables[]): TimeSpan {
+    const minStartTime = stageTimetables
+      .flatMap(x => x.timetables.map(x => new Date(x.start_time).getTime()))
+      .sort()[0];
+
+    const maxEndTime = stageTimetables
+      .flatMap(x => x.timetables.map(x => new Date(x.end_time).getTime()))
+      .sort((a, b) => b - a)[0];
+
+    const intervalInHours = Math.ceil((maxEndTime - minStartTime) / (1000 * 60 * 60))
+
+    return {
+      minStartTime,
+      maxEndTime,
+      intervalInHours
+    }
+
   }
 
   onDayFilterChange(event: Event): void {
