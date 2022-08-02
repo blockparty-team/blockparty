f394f969840079fc5294da5c1f989f72a7625274
baffioso <baffyx@gmail.com>
2022-08-02 23:51:23 +0200

    Rename of storage to preferences

--- src/app/services/device-storage.service.ts
+++ src/app/services/device-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Storage } from '@capacitor/storage';
+import { Preferences } from '@capacitor/preferences';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -11,14 +11,14 @@ export type StorageKeys = 'artists' | 'favorites' | 'timetable';
 export class DeviceStorageService {
 
   set(key: StorageKeys, value: any): void {
-    Storage.set({
+    Preferences.set({
       key,
       value: JSON.stringify(value)
     });
   }
 
   get(key: StorageKeys): Observable<any> {
-    return from(Storage.get({ key })).pipe(
+    return from(Preferences.get({ key })).pipe(
       map(prop => JSON.parse(prop.value))
     );
   }
