3e8cd039807954f91ed7adfbc0415bb9b6913db7
baffioso <baffyx@gmail.com>
2022-10-01 09:22:04 +0200

    Trigger play from ts + dropshadow on logos

--- src/app/pages/partners/partners.page.ts
+++ src/app/pages/partners/partners.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.page.html',
   styleUrls: ['./partners.page.scss'],
 })
-export class PartnersPage implements OnInit {
+export class PartnersPage implements AfterViewInit {
+
+  @ViewChild('video') video: ElementRef<HTMLVideoElement>
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.video.nativeElement.play();
   }
 
 }
