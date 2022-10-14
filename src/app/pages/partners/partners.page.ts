d5e1fd2e85d47d04568c91cded7d2f90996415cf
Anders Barfod <baffyx@gmail.com>
2022-10-14 19:07:13 +0200

    Removed programmaticly autoplay

--- src/app/pages/partners/partners.page.ts
+++ src/app/pages/partners/partners.page.ts
@@ -1,18 +1,12 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.page.html',
   styleUrls: ['./partners.page.scss'],
 })
-export class PartnersPage implements AfterViewInit {
-
-  @ViewChild('video') video: ElementRef<HTMLVideoElement>
+export class PartnersPage {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-    this.video.nativeElement.play();
-  }
 
 }
