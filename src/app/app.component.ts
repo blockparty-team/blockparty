ff30c9756e4513caa8153ca5cfad75cb18cf3427
Anders Barfod <baffyx@gmail.com>
2022-10-19 00:43:43 +0100

    Changed to OneSignal angular module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { StatusBar } from '@capacitor/status-bar'
 import { Device } from '@capacitor/device';
+import { OneSignal } from 'onesignal-ngx';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,19 @@ import { Device } from '@capacitor/device';
 })
 export class AppComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private oneSignal: OneSignal
+  ) { }
 
   ngOnInit(): void {
     Device.getInfo().then(info => {
       if (info.platform !== 'web') {
         StatusBar.setBackgroundColor({ color: '#443f3f' })
       }
+    })
+
+    this.oneSignal.init({
+      appId: '0ac7afcb-7e95-4642-8d66-f149d173cc5b'
     })
   }
 }
