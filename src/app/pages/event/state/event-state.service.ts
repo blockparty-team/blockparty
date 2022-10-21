136536c7e62ee5c034fbaf42d22b93f3e5f4bdaa
Anders Barfod <baffyx@gmail.com>
2022-10-21 22:48:49 +0100

    Added days and artists to event list/detail

--- src/app/pages/event/state/event-state.service.ts
+++ src/app/pages/event/state/event-state.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
+import { EventViewModel } from '@app/interfaces/event';
 import { pathToImageUrl } from '@app/shared/utils';
 import { StoreService } from '@app/store/store.service';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventStateService {
 
-  events$ = this.store.events$.pipe(
-    map(events => events.map(event => ({...event, imgUrl: pathToImageUrl(event.storage_path)})))
+  events$: Observable<EventViewModel[]> = this.store.events$.pipe(
+    map(events => events.map(event => {
+      // Make artist and day root properties
+      const {day_event, stage, ...rest} = event
+      return {
+        ...rest,
+        artists: stage
+          .flatMap(stage => stage.timetable.map(t => t.artist))
+          .sort((a, b) =>  a.name.localeCompare(b.name)),
+        days: day_event.map(day => day.day.name)
+      }
+    })),
+    map(events => events.map(event => ({ ...event, imgUrl: pathToImageUrl(event.storage_path) })))
   )
-  
+
   constructor(
     private store: StoreService
   ) { }
