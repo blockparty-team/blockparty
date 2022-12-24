f84e6f6a5abde2db01fb29f409abbf82533567cc
Anders Barfod <baffyx@gmail.com>
2022-12-24 08:42:12 +0100

    Animation on initial map load (#29)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { LottieModule } from 'ngx-lottie';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { environment } from '@env/environment';
 import { ServiceWorkerModule } from '@angular/service-worker';
+
+export function playerFactory() {
+  return import(/* webpackChunkName: 'lottie-web' */ 'lottie-web');
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,10 +26,10 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
+    LottieModule.forRoot({ player: playerFactory })
   ],
-
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/pages/map/map.module.ts
+++ src/app/pages/map/map.module.ts
@@ -2,8 +2,9 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MapPage } from './map.page';
+import { LottieModule } from 'ngx-lottie';
 
+import { MapPage } from './map.page';
 import { MapPageRoutingModule } from './map-routing.module';
 import { StageTimetableComponent } from '@app/pages/map/feature-info-modal/stage-timetable/stage-timetable.component';
 import { FeatureInfoModalComponent } from './feature-info-modal/feature-info-modal.component';
@@ -14,7 +15,8 @@ import { AssetComponent } from './feature-info-modal/asset/asset.component';
     IonicModule,
     CommonModule,
     FormsModule,
-    MapPageRoutingModule
+    MapPageRoutingModule,
+    LottieModule
   ],
   declarations: [
     MapPage,
