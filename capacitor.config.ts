1a09e96c95defe716c357fb813ba767a1bf8c779
Anders Barfod <baffyx@gmail.com>
2023-01-10 23:41:31 +0100

    52 add ios (#79)
    * Add ios
    
    * Recommended changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.vercel.distortioncph',
+  appId: 'com.cphdistortion.app',
   appName: 'Distortion',
   webDir: 'www',
   bundledWebRuntime: false
