7df2667e63fe4540682e6c10cdf4e82e133dc540
Anders Barfod <baffyx@gmail.com>
2023-01-28 22:23:34 +0100

    6 push notifications (#90)
    * installed onesignal plugin
    
    * Update to targe android 13
    
    * IOS push working
    
    * Push notification service for initializing native and web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component, NgZone, OnInit } from '@angular/core';
 import { StatusBar } from '@capacitor/status-bar'
 import { Device } from '@capacitor/device';
-import { OneSignal } from 'onesignal-ngx';
-import { environment } from '@env/environment';
 import { SupabaseService } from './services/supabase.service';
 import { App, URLOpenListenerEvent } from "@capacitor/app";
 import { Router } from '@angular/router';
 import { RouteHistoryService as RouteHistoryService } from './services/routeHistory.service';
+import { PushNotificationService } from './services/push-notification.service';
 
 @Component({
   selector: 'app-root',
@@ -19,23 +18,22 @@ export class AppComponent implements OnInit {
   constructor(
     private zone: NgZone,
     private router: Router,
-    private oneSignal: OneSignal,
     private supabase: SupabaseService,
-    private routeHistoryService: RouteHistoryService
+    private routeHistoryService: RouteHistoryService,
+    private pushNotificationService: PushNotificationService
   ) {
-    this.oneSignal.init({
-      appId: environment.oneSignalAppId,
-      allowLocalhostAsSecureOrigin: true
-    })
-
     this.setupAppUrlOpenListener();
   }
 
   ngOnInit(): void {
     Device.getInfo().then(info => {
       if (info.platform !== 'web') {
-        StatusBar.setBackgroundColor({ color: '#443f3f' })
+        StatusBar.setBackgroundColor({ color: '#443f3f' });
       }
+    });
+
+    Device.getId().then(deviceId => {
+      this.pushNotificationService.initOneSignal(deviceId.uuid);
     })
 
     this.supabase.authChanges((event, session) => {
@@ -44,21 +42,19 @@ export class AppComponent implements OnInit {
       } else {
         this.supabase.setSession(null);
       }
-    })
+    });
 
     this.routeHistoryService.init();
-
   }
 
+  // Url listener extracting tokens when getting auth redirect on ios/android native 
   setupAppUrlOpenListener() {
     App.addListener("appUrlOpen", async (urlData: URLOpenListenerEvent) => {
-      console.log("       ", urlData);
-
       const openUrl = urlData.url;
       const access = openUrl.split("#access_token=").pop().split("&")[0];
       const refresh = openUrl.split("&refresh_token=").pop().split("&")[0];
 
-      const {data, error} = await this.supabase.externalSetSession(access, refresh);
+      const { data, error } = await this.supabase.externalSetSession(access, refresh);
       this.supabase.setSession(data.session);
 
       this.zone.run(() => {
---
+++ src/app/services/push-notification.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { environment } from '@env/environment';
+import OneSignal from 'onesignal-cordova-plugin';
+import { OneSignal as OneSignalWeb } from 'onesignal-ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PushNotificationService {
+
+  constructor(
+    private oneSignalWeb: OneSignalWeb
+  ) { }
+
+  private initOneSignalNative(externalUserId?: string): void {
+
+    OneSignal.setAppId(environment.oneSignalAppId);
+
+    if (externalUserId) {
+      OneSignal.setExternalUserId(externalUserId);
+    }
+
+    OneSignal.setNotificationOpenedHandler((jsonData) => {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    });
+    
+    OneSignal.promptForPushNotificationsWithUserResponse((accepted) => {
+      console.log("User accepted notifications: " + accepted);
+    });
+  }
+
+  private initOneSignalWeb(): void {
+    this.oneSignalWeb.init({
+      appId: environment.oneSignalAppId,
+      allowLocalhostAsSecureOrigin: true
+    });
+  }
+
+  public initOneSignal(externalUserId?: string): void {
+    Device.getInfo().then(info => {
+      if (info.platform === 'web') {
+        this.initOneSignalWeb();
+      } else {
+        this.initOneSignalNative(externalUserId);
+      }
+    });
+  }
+
+}
