01ac83b77fe0245dfbd9f0bf7615bcc331f5faee
Anders Barfod <baffyx@gmail.com>
2023-01-29 13:49:22 +0100

    Show stage description on map feature info

--- src/app/pages/map/feature-info-modal/stage-timetable/stage-timetable.component.ts
+++ src/app/pages/map/feature-info-modal/stage-timetable/stage-timetable.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MapStateService } from '@app/pages/map/state/map-state.service';
 import { ModalController, SegmentCustomEvent } from '@ionic/angular';
+import { MapStateService } from '@app/pages/map/state/map-state.service';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map, shareReplay, tap } from 'rxjs/operators';
 import { MapLayer } from '@app/interfaces/map-layer';
 import { Day, StageGeojsonProperties, Timetable, TimetableDay } from '@app/interfaces/stage-geojson-properties';
 import { MapClickedFeature } from '@app/interfaces/map-clicked-feature';
@@ -17,8 +17,10 @@ import { MapClickedFeature } from '@app/interfaces/map-clicked-feature';
 export class StageTimetableComponent implements OnInit {
 
   stageName$: Observable<string>;
+  stageDescription$: Observable<string>;
   days$: Observable<Day[]>;
   timetable$: Observable<Timetable[]>;
+  hasTimetable$: Observable<boolean>;
   location$: Observable<[number, number]>;
 
   private _selectedDay$ = new BehaviorSubject<string>(null);
@@ -49,13 +51,19 @@ export class StageTimetableComponent implements OnInit {
       map(stage => stage.properties.name)
     );
 
+    this.stageDescription$ = stage$.pipe(
+      map(stage => stage.properties.description)
+    );
+
     this.days$ = stage$.pipe(
+      // If no timetables assigned to stage, backend returns [null] for timetables array
+      filter(stage => stage.properties.timetables[0] !== null),
       map(stage => stage.properties.timetables
         .map(t => t.day)
         .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
       ),
       tap(days => this._selectedDay$.next(days[0].id))
-    )
+    );
 
     this.timetable$ = combineLatest([
       stage$,
@@ -66,7 +74,12 @@ export class StageTimetableComponent implements OnInit {
         stage.properties.timetables
           .find(timetable => timetable.day.id === day).timetable
       ))
-    )
+    );
+
+    // If no timetables assigned to stage, backend returns [null] for timetables array
+    this.hasTimetable$ = stage$.pipe(
+      map(stage => stage.properties.timetables[0] !== null)
+    );
 
     this.location$ = stage$.pipe(
       map(stage => [
@@ -85,11 +98,13 @@ export class StageTimetableComponent implements OnInit {
     this.router.navigate(['/tabs/', 'artist', name]);
   }
 
-  onOpenGoogleMapseDirections(coords: [number, number]): void {
-
+  onOpenGoogleMapsDirections(coords: [number, number]): void {
     const url = `https://www.google.com/maps/dir/?api=1&travelmode=walking&destination=${coords[0]},${coords[1]}`;
-
     window.open(url, '_blank');
+  }
+
+  onCloseStageDescriptionModal() {
+    this.modalCtrl.dismiss();
   }
 
 }
