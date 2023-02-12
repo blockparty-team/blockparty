4fcea686809e87e7eb17e47338fa1c58c96bed96
Anders Barfod <baffyx@gmail.com>
2023-02-12 07:48:46 +0100

    Changed bundle identifier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.cphdistortion.app',
+  appId: 'dk.cphdistortion.app',
   appName: 'Distortion',
   webDir: 'www',
   bundledWebRuntime: false
