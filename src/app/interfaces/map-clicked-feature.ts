acdb5cc365366dd25c13a11b2d564b4abae50373
Anders Barfod <baffyx@gmail.com>
2023-04-15 08:21:58 +0200

    Use mask geojson instead of mask entity (#249)

--- src/app/interfaces/map-clicked-feature.ts
+++ src/app/interfaces/map-clicked-feature.ts
@@ -2,8 +2,9 @@ import { Point } from 'geojson';
 import { MapLayer } from './map-layer';
 import { StageGeojsonProperties } from './stage-geojson-properties';
 import { AssetGeojsonProperties } from './asset-geojson-properties';
+import { MaskGeojsonProperties } from './mask-geojson-properties';
 
-export type GeojsonProperties = StageGeojsonProperties | AssetGeojsonProperties;
+export type GeojsonProperties = StageGeojsonProperties | AssetGeojsonProperties | MaskGeojsonProperties;
 
 export interface MapClickedFeature<T> {
     id: string;
