0f0599da2071be35b8db706e0eb2567fe05525d2
Anders Barfod <baffyx@gmail.com>
2023-05-13 11:45:17 +0200

    Add google directions link on asset

--- src/app/pages/map/feature-info-modal/asset/asset.component.ts
+++ src/app/pages/map/feature-info-modal/asset/asset.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MapClickedFeature } from '@app/interfaces/map-clicked-feature';
 import { MapLayer } from '@app/interfaces/map-layer';
 import { Observable } from 'rxjs';
@@ -11,21 +11,24 @@ import { MapStateService } from '../../state/map-state.service';
   styleUrls: ['./asset.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AssetComponent implements OnInit {
+export class AssetComponent {
 
-  asset$: Observable<MapClickedFeature<any>>
+  private mapStateService = inject(MapStateService);
 
-  constructor(
-    private mapStateService: MapStateService
-  ) { }
+  asset$: Observable<MapClickedFeature<any>> = this.mapStateService.selectedMapFeature$.pipe(
+    filter(feature => (
+      feature.mapLayer === MapLayer.Asset ||
+      feature.mapLayer === MapLayer.AssetIcon
+    ))
+  );
 
-  ngOnInit() {
-    this.asset$ = this.mapStateService.selectedMapFeature$.pipe(
-      filter(feature => (
-        feature.mapLayer === MapLayer.Asset ||
-        feature.mapLayer === MapLayer.AssetIcon
-      ))
-    )
+  location$: Observable<[number, number]> = this.asset$.pipe(
+    map(stage => stage.geometry.coordinates as [number, number])
+  );
+
+  onOpenGoogleMapsDirections(coords: [number, number]): void {
+    const url = `https://www.google.com/maps/dir/?api=1&travelmode=walking&destination=${coords[1]},${coords[0]}`;
+    window.open(url, '_blank');
   }
 
 }
