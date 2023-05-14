16d45261ab6ff1a038976ee5a06f64244b9f4ad5
Anders Barfod <baffyx@gmail.com>
2023-05-14 21:05:40 +0200

    Close modal on map interaction and hide event filter on map interaction (#320)

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -23,23 +23,23 @@ export const animations = {
     fadeInOut: [
         trigger('fadeInOut', [
             transition(':enter', [
-              style({ opacity: 0 }),
-              animate(600, style({ opacity: 1 }))
+                style({ opacity: 0 }),
+                animate(300, style({ opacity: 1 }))
             ]),
             transition('* => void', [
-              animate(600, style({ opacity: 0 }))
+                animate(300, style({ opacity: 0 }))
             ])
-          ])
+        ])
     ],
     slideUpDown: [
         trigger('slideUpDown', [
             transition(':enter', [
-                style({ transform: 'translateY(100%)' }),
-                animate('200ms ease-in', style({ transform: 'translateY(0%)' }))
+                style({ transform: 'translateY(-100%)' }),
+                animate('300ms ease-in', style({ transform: 'translateY(0%)' }))
             ]),
-            transition(':leave', [
-                animate('200ms ease-in', style({ transform: 'translateY(100%)' }))
+            transition('* => void', [
+                animate(300, style({ transform: 'translateY(100%)' }))
             ])
         ])
-    ],
+    ]
 }
