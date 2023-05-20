2ff0b30f07f29c5b3ddb96e14b43f562c91b7b91
Anders Barfod <baffyx@gmail.com>
2023-05-20 07:11:03 +0200

    Update transport with viggo content

--- src/app/pages/transport/transport.page.ts
+++ src/app/pages/transport/transport.page.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { RouteHistoryService } from '@app/services/routeHistory.service';
 import { map } from 'rxjs';
 import { Clipboard } from '@capacitor/clipboard';
+import { Device } from '@capacitor/device';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-transport',
@@ -20,5 +22,25 @@ export class TransportPage {
 
   onCopyVoucherCode() {
     Clipboard.write({string: this.viggoVoucherCode});
+  }
+
+  onGoToViggo() {
+    Device.getInfo().then(info => {
+      let url: string;
+
+      switch (info.platform) {
+        case 'ios':
+          url = 'https://apps.apple.com/dk/app/viggo-book-et-lift/id1460781653'
+          break;
+        case 'android':
+          url = 'https://play.google.com/store/apps/details?id=io.viggo.rider'
+          break
+        default:
+          url = 'https://www.viggo.com'
+          break;
+      }
+
+      Browser.open({url})
+    });
   }
 }
