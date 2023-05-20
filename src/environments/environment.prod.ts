9fd65dabba282050fc0b2afa7355178c4044e993
Anders Barfod <baffyx@gmail.com>
2023-05-20 22:00:01 +0200

    Changed to nusnus maptiler style and initial map view (#346)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
   appUrl: 'https://distortioncph.vercel.app',
   supabaseUrl: 'https://uvyohxoytqcqogahbcwo.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2eW9oeG95dHFjcW9nYWhiY3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkyMzAxNzUsImV4cCI6MTk5NDgwNjE3NX0.3B8cKWbwLGPVQsAas1ZmhEtp4WsnSOMTfmEAhFz6wvg',
-  maptilerStyleJson: 'https://api.maptiler.com/maps/decadbf9-1a07-4b7b-9726-fed2f003b673/style.json?key=MZCjtFvEvhy0zEdhtmhp',
+  maptilerStyleJson: 'https://api.maptiler.com/maps/62517ef6-259e-4ecc-a9df-6b330f8b5092/style.json?key=IFNEgQuIP7z1PzGMheNY',
   mapView: {
-    center: [12.547927, 55.667071],
-    zoom: 14,
+    center: [12.57632, 55.68215],
+    zoom: 11.5,
     pitch: 0
   },
   oneSignalAppId: '1c3348ed-5747-4c3d-89f1-f6ad3049c0a0',
