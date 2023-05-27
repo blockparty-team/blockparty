18c27a7ae617d5f60f95faf03e5034cfb22de846
Anders Barfod <baffyx@gmail.com>
2023-05-27 14:32:17 +0200

    Custom maptiler basemap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,7 @@ export const environment: Environment = {
   supabaseUrl: 'https://uvyohxoytqcqogahbcwo.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2eW9oeG95dHFjcW9nYWhiY3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkyMzAxNzUsImV4cCI6MTk5NDgwNjE3NX0.3B8cKWbwLGPVQsAas1ZmhEtp4WsnSOMTfmEAhFz6wvg',
   maptilerStyleJson: 'https://api.maptiler.com/maps/62517ef6-259e-4ecc-a9df-6b330f8b5092/style.json?key=IFNEgQuIP7z1PzGMheNY',
+  maptilerApiKey: 'IFNEgQuIP7z1PzGMheNY',
   mapView: {
     center: [12.57632, 55.68215],
     zoom: 11.5,
