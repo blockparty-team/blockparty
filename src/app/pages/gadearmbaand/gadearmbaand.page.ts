0e8aa743e40007826e73d9ed2a9d4d1dffd213e4
Anders Barfod <baffyx@gmail.com>
2023-05-29 21:18:31 +0200

    Removed unused code

--- src/app/pages/gadearmbaand/gadearmbaand.page.ts
+++ src/app/pages/gadearmbaand/gadearmbaand.page.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gadearmbaand',
   templateUrl: './gadearmbaand.page.html',
   styleUrls: ['./gadearmbaand.page.scss'],
 })
-export class GadearmbaandPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class GadearmbaandPage { }
