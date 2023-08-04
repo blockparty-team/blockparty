69ab53958c1c932c96c3b50cafa43bd5aed0ae53
Anders Barfod <baffyx@gmail.com>
2023-08-04 07:24:24 +0200

    Environment variables

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -7,14 +7,14 @@ import { Environment } from "./environment.model";
 // The list of file replacements can be found in `angular.json`.
 export const environment: Environment = {
   production: false,
-  festivalName: 'Distortion Festival',
+  festivalName: 'Karrusel Festival',
   appUrl: 'http://localhost:8100',
-  supabaseUrl: 'https://uvyohxoytqcqogahbcwo.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2eW9oeG95dHFjcW9nYWhiY3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkyMzAxNzUsImV4cCI6MTk5NDgwNjE3NX0.3B8cKWbwLGPVQsAas1ZmhEtp4WsnSOMTfmEAhFz6wvg',
+  supabaseUrl: 'http://localhost:54321',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
   maptilerStyleJson: 'https://api.maptiler.com/maps/decadbf9-1a07-4b7b-9726-fed2f003b673/style.json?key=MZCjtFvEvhy0zEdhtmhp',
   mapView: {
-    center: [12.547927, 55.667071],
-    zoom: 14,
+    center: [12.618371, 55.690323],
+    zoom: 16.5,
     pitch: 0
   },
   oneSignalAppId: 'e5df19f0-b1e1-4e2d-bbc0-3e7c8437fdc4',
