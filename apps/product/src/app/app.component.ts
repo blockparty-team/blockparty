f1389fcd987e4ad4cb4f42f8a3bf37e422fa7d0b
Anders Barfod <nw3k@kk.dk>
2024-01-25 07:25:49 +0100

    product: more section and about

--- apps/product/src/app/app.component.ts
+++ apps/product/src/app/app.component.ts
@@ -76,6 +76,7 @@ export class AppComponent implements OnInit {
       image: 'assets/editor.png',
       svgPath: 'm16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125'
     },
+
     {
       title: 'Analytics',
       bullets: [
@@ -88,10 +89,43 @@ export class AppComponent implements OnInit {
       image: 'assets/analytics.png',
       svgPath: 'M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z'
     },
+    {
+      title: 'Others',
+      bullets: [
+        'Partner branding',
+        'Push notifications',
+        'Custom pages',
+      ],
+      backgroundColor: 'bg-green-300',
+      color: 'text-army-600',
+      image: 'assets/more.png',
+      svgPath: 'M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z'
+    },
   ])
+
+  copied = signal<boolean>(false);
 
   ngOnInit(): void {
     AOS.init()
   }
+
+  onCopyMail(): void {
+
+    try {
+      const mail = 'blockparty.dev@proton.me';
+      navigator.clipboard.writeText(mail);
+      this.copied.set(true);
+      setTimeout(() => {
+        this.copied.set(false);
+      }, 2000);
+    } catch (error) {
+     this.copied.set(false)
+    }
+
+
+
+
+  }
+
 
 }
