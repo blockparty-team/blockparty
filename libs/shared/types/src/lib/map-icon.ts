31271300a4e21fbf5bc91c678f771b8346d42c28
Anders Barfod <nw3k@kk.dk>
2024-07-21 10:45:33 +0200

    Removed dublicate types lib

--- libs/shared/types/src/lib/map-icon.ts
+++
@@ -1,6 +0,0 @@
-import { Tables } from '@blockparty/distortion/data-access/supabase';
-
-export interface MapIconViewModel extends Tables<'map_icon'> {
-  fileUrl: string | null;
-  image: HTMLImageElement | null;
-}
