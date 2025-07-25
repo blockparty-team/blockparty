dda478da85877f6e507822c52d40d427c002fc0b
Anders Barfod <nw3k@kk.dk>
2025-07-25 21:08:24 +0200

    feat: update about page content and remove unused pages and routes

--- apps/karrusel/src/app/pages/gadearmbaand/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { GadearmbaandPage } from './gadearmbaand.page';
-
-export const routes: Route[] = [
-  {
-    path: '',
-    component: GadearmbaandPage,
-  },
-];
